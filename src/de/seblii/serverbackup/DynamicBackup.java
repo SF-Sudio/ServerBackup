b00b06b3b7fea4e4d08ba04bb33140d8a067b94c
NickBonet <nick.b98@Hotmail.com>
2023-09-21 10:30:28 -0400

    refactor DynamicBackup, confirm it works with the Nether/The End.

--- src/de/seblii/serverbackup/DynamicBackup.java
+++ src/de/seblii/serverbackup/DynamicBackup.java
@@ -8,81 +8,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 
 public class DynamicBackup implements Listener {
 
-	List<Chunk> chunks = Collections.synchronizedList(new ArrayList<>());
-	public boolean isSaving = false;
-
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
-		if (ServerBackup.getInstance().getConfig().getBoolean("DynamicBackup")) {
-			if (e.getFrom().getChunk() != e.getTo().getChunk()) {
-				int regX = e.getTo().getChunk().getX() >> 5;
-				int regZ = e.getTo().getChunk().getZ() >> 5;
+		if (ServerBackup.getInstance().getConfig().getBoolean("DynamicBackup") && (e.getFrom().getChunk() != Objects.requireNonNull(e.getTo()).getChunk())) {
+				// region X & Y (mca file)
+				Chunk chunk = e.getTo().getChunk();
+				int regX = chunk.getX() >> 5;
+				int regZ = chunk.getZ() >> 5;
 
-				Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), new Runnable() {
 
-					@Override
-					public void run() {
-						String chunkInf = "Data." + e.getTo().getWorld().getName() + ".Chunk." + "r." + regX + "."
-								+ regZ + ".mca";
-
-						if (!Bukkit.getWorldContainer().toString().equalsIgnoreCase(".")) {
-							chunkInf = "Data." + Bukkit.getWorldContainer() + "\\" + e.getTo().getWorld().getName()
-									+ ".Chunk." + "r." + regX + "." + regZ + ".mca";
-						}
-
-						if (!chunks.contains(e.getTo().getChunk())) {
-							if (!ServerBackup.getInstance().bpInf.contains(chunkInf)) {
-								chunks.add(e.getTo().getChunk());
-								ServerBackup.getInstance().bpInf.set(chunkInf, chunks.get(chunks.size() - 1).getX());
-								ServerBackup.getInstance().bpInf.set(chunkInf, chunks.get(chunks.size() - 1).getZ());
-
-								saveChanges();
-								try {
-									chunks.remove(e.getTo().getChunk());
-								} catch (ArrayIndexOutOfBoundsException ex) {
-								}
-							} else {
-								chunks.add(e.getTo().getChunk());
-								ServerBackup.getInstance().bpInf.set(chunkInf, chunks.get(chunks.size() - 1).getWorld());
-
-								saveChanges();
-								try {
-									chunks.remove(e.getTo().getChunk());
-								} catch (ArrayIndexOutOfBoundsException ex) {
-								}
-							}
-						}
-					}
-
+				Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), () -> {
+                    String chunkInf = getChunkInfoPath(Objects.requireNonNull(e.getTo().getWorld()).getName(), regX, regZ);
+					addChunkToBackupInfo(chunk, chunkInf, e.getPlayer().getName(), chunk.getWorld().getName());
 				});
-			}
-		}
-	}
 
-	public void saveChanges() {
-		if (!isSaving) {
-			isSaving = true;
-
-			Bukkit.getScheduler().runTaskLaterAsynchronously(ServerBackup.getInstance(), new Runnable() {
-
-				@Override
-				public void run() {
-					ServerBackup.getInstance().saveBpInf();
-					if (ServerBackup.getInstance().getConfig().getBoolean("SendLogMessages")) {
-						Bukkit.getLogger().log(Level.INFO, "DynamicBP: file saved.");
-					}
-
-					isSaving = false;
-				}
-
-			}, 20 * 5);
 		}
 	}
 
@@ -90,42 +34,37 @@ public class DynamicBackup implements Listener {
 	public void onJoin(PlayerJoinEvent e) {
 		if (ServerBackup.getInstance().getConfig().getBoolean("DynamicBackup")) {
 			Player p = e.getPlayer();
+			Chunk chunk = p.getLocation().getChunk();
+			int regX = chunk.getX() >> 5;
+			int regZ = chunk.getZ() >> 5;
 
-			int regX = p.getLocation().getChunk().getX() >> 5;
-			int regZ = p.getLocation().getChunk().getZ() >> 5;
+			Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), () -> {
+				String chunkInf = getChunkInfoPath(Objects.requireNonNull(p.getLocation().getWorld()).getName(), regX, regZ);
 
-			Bukkit.getScheduler().runTaskAsynchronously(ServerBackup.getInstance(), new Runnable() {
-
-				@Override
-				public void run() {
-					String chunkInf = "Data." + p.getLocation().getWorld().getName() + ".Chunk." + "r." + regX + "."
-							+ regZ + ".mca";
-
-					if (!Bukkit.getWorldContainer().toString().equalsIgnoreCase(".")) {
-						chunkInf = "Data." + Bukkit.getWorldContainer() + "\\" + p.getLocation().getWorld().getName()
-								+ ".Chunk." + "r." + regX + "." + regZ + ".mca";
-					}
-
-					if (!chunks.contains(p.getLocation().getChunk())) {
-						if (!ServerBackup.getInstance().bpInf.contains(chunkInf)) {
-							chunks.add(p.getLocation().getChunk());
-							ServerBackup.getInstance().bpInf.set(chunkInf + ".X", p.getLocation().getChunk().getX());
-							ServerBackup.getInstance().bpInf.set(chunkInf + ".Z", p.getLocation().getChunk().getZ());
-
-							ServerBackup.getInstance().saveBpInf();
-							chunks.remove(p.getLocation().getChunk());
-						} else {
-							chunks.add(p.getLocation().getChunk());
-							ServerBackup.getInstance().bpInf.set(chunkInf, p.getLocation().getChunk().getWorld());
-
-							ServerBackup.getInstance().saveBpInf();
-							chunks.remove(p.getLocation().getChunk());
-						}
-					}
-				}
-
+				addChunkToBackupInfo(chunk, chunkInf, p.getName(), chunk.getWorld().getName());
 			});
 		}
 	}
 
+	private static String getChunkInfoPath(String worldName, int regX, int regZ) {
+		String chunkInf = "Data." + worldName + ".Chunk." + "r." + regX + "."
+				+ regZ + ".mca";
+
+		if (!Bukkit.getWorldContainer().toString().equalsIgnoreCase(".")) {
+			chunkInf = "Data." + Bukkit.getWorldContainer() + "\\" + worldName
+					+ ".Chunk." + "r." + regX + "." + regZ + ".mca";
+		}
+		return chunkInf;
+	}
+
+	private static void addChunkToBackupInfo(Chunk chunk, String chunkInf, String playerName, String worldName) {
+		if (!ServerBackup.getInstance().bpInf.contains(chunkInf)) {
+			ServerBackup.getInstance().bpInf.set(chunkInf + ".chunkX", chunk.getX());
+			ServerBackup.getInstance().bpInf.set(chunkInf + ".chunkZ", chunk.getZ());
+			ServerBackup.getInstance().bpInf.set(chunkInf + ".triggeredBy", playerName);
+			ServerBackup.getInstance().saveBackupInfoChanges();
+			Bukkit.getLogger().log(Level.INFO, "Region {0}, {1} added to backup set for {2}.",
+					new Object[]{chunk.getX() >> 5, chunk.getZ() >> 5, worldName});
+		}
+	}
 }
